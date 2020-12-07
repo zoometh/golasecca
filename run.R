067a7e107db3068768e26352935eb0848744574f
Shennan <thomashuet7@gmail.com>
2020-12-07 20:58:21 +0100

    index

--- run.R
+++ run.R
@@ -1,7 +1,7 @@
-# run different variable
-# to create leaflet .html ouputs
-source("func/functions.R") # load/run ? function
-# chm <- getwd()
-for(per in c("GIC_IIAB","GIIAB_IIIA1","GIIIA1_IIIA3","GIIIA3")){
-  f.lflt.per(getwd(),tab,per) # create leaflet html map 
-}
+# # run different variable
+# # to create leaflet .html ouputs
+# source("func/functions.R") # load/run ? function
+# # chm <- getwd()
+# for(per in c("GIC_IIAB","GIIAB_IIIA1","GIIIA1_IIIA3","GIIIA3")){
+#   f.lflt.per(getwd(),tab,per) # create leaflet html map 
+# }
